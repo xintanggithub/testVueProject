c647ae416f90b5179dcb5ea7abd8186ec1373bad
tsontang <tsontang@jidouauto.com>
2019-11-05 17:47:51 +0800

    新增请求工具类

---
+++ src/utils/request.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:8080';
+
+const service = axios.create({
+    baseURL: baseUrl,
+    timeout: 5000
+});
+
+service.interceptors.request.use(config => {
+    //可以处理配置信息（请求头、参数加密等）
+    return config
+}, error => {
+    console.log("request error", error);
+    Promise.reject(error)
+});
+
+service.interceptors.response.use(response => {
+        const res = response.data;
+        if (res.resultCode !== 200) {
+            this.$notify({
+                title: 'error',
+                type: 'error',
+                message: res.resultMessage || res.msg,
+                duration: 2000
+            })
+        } else {
+            return response
+        }
+    },
+    error => {
+        console.log("response error", error);
+        return Promise.reject(error)
+    });
