72ade02ccc90f406f0182b73ff475bfac1a1c5aa
tsontang <tsontang@jidouauto.com>
2020-01-09 18:30:06 +0800

    change1

--- src/page/children/book/book.vue
+++ src/page/children/book/book.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        tab---aaa -->
+        <div v-for="(itemData,index) in listData" :key="index">
+            <div style="width: 18vw;height: 20vh;">
+                {{itemData.title}}
+            </div>
+        </div>
     </div>
 </template>
 <script>
     export default {
-        name: 'book'
+        name: 'book',
+        data() {
+            return {
+                listData: [
+                    {title: "1"},
+                    {title: "2"},
+                    {title: "3"},
+                    {title: "4"},
+                ]
+            }
+        }
     }
 
 </script>
