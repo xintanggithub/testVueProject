2872664a4e27dadcb5a93cfa7e2441e95fc3c369
tsontang <tsontang@jidouauto.com>
2020-02-18 18:12:40 +0800

    game功能完成

--- src/page/children/game/game.vue
+++ src/page/children/game/game.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-        <!--list-->
-        <div style="margin-top: 20px;align-items: center;word-wrap: break-word;word-break: break-all;overflow: hidden;">
-            <div v-for="item in listData" style="width: 15vw;height: 20vh;background-color: aquamarine;margin: 10px;">
-                {{item.title}}
+        <div style="padding-top:2vh;width: 100%;height: 5vh;">
+            <div @mouseover="mouseover" @mouseout="mouseout" class="search" :style="'width:'+(10+0.8*transitionCount)+'vw;'">
+                <el-input placeholder="请输入搜索内容" v-model="keyword" @keyup.enter.native="queryGameList()">
+                    <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                </el-input>
             </div>
         </div>
-        <div class="block">
+        <!--list-->
+        <div class="top_list_root">
+            <div class="list_root_style">
+                <div v-for="(itemData,index) in listData" :key="index">
+                    <el-card class="list_item"
+                             shadow="hover">
+                        <el-image @click="openItem(itemData)" fit="cover"
+                                  class="img_item" :src="itemData.img"></el-image>
+                        <div @click="openItem(itemData)" style="display: flex;flex-direction: column;">
+                            <div>
+                                <el-link type="warning">{{itemData.title}}</el-link>
+                            </div>
+                            <div>
+                                <el-tag type="warning" size="mini" :style="index!==0?'margin-left: 0.5vw;':''"
+                                        :key="index"
+                                        v-for="(tag,index) in loadTag(itemData.tag)" :disable-transitions="false">
+                                    <i v-show="index===0" class="el-icon-collection-tag"></i>
+                                    {{tag}}
+                                </el-tag>
+                            </div>
+                        </div>
+                    </el-card>
+                </div>
+            </div>
+        </div>
+        <div class="block mg_pg">
             <el-pagination
                     @current-change="handleCurrentChange"
                     :current-page.sync="page"
@@ -25,6 +51,8 @@
         name: 'game',
         data() {
             return {
+                transitionCount: 0,
+                t2: null,
                 total: 0,
                 page: 1,
                 pageSize: 18,
@@ -53,9 +81,95 @@
                     console.log("success==> " + error)
                 })
             },
+            loadTag(val) {
+                if (null === val || "" === val || undefined === val) {
+                    return ["暂无标签"]
+                }
+                return val.split(",");
+            },
+            openItem(val) {
+                window.open(val.html);
+            },
+            mouseover() {
+                console.log("====> mouseover ====>");
+                this.transitionCount = 0;
+                this.clearIntervalMD();
+                this.drawTransition(1);
+            },
+            mouseout() {
+                console.log("====> mouseout ====>");
+                this.clearIntervalMD();
+                this.drawTransition(2);
+            },
+            clearIntervalMD() {
+                if (null !== this.t2) {
+                    window.clearInterval(this.t2);
+                    this.t2 = null;
+                }
+            },
+            drawTransition(type) {
+                let v = this;
+                this.t2 = setInterval(function () {
+                    if (type === 1) {
+                        v.transitionCount = v.transitionCount + 1;
+                        if (v.transitionCount >= 40) {
+                            window.clearInterval(v.t2);
+                            v.t2 = null;
+                        }
+                    } else {
+                        v.transitionCount = v.transitionCount - 1;
+                        if (v.transitionCount <= 0) {
+                            window.clearInterval(v.t2);
+                            v.t2 = null;
+                        }
+                    }
+                }, 5);
+            },
         },
         created() {
             this.queryGameList();
         }
     }
 </script>
+<style>
+    .list_root_style {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap
+    }
+
+    .list_item {
+        width: 15vw;
+        height: 22.7vh;
+        margin-left: 1vw;
+        margin-top: 1vh;
+        margin-bottom: 1vh;
+    }
+
+    .img_item {
+        width: 13vw;
+        height: 14vh;
+        border-radius: 5px 5px 0 0
+    }
+
+    .top_list_root {
+        margin-left: 1vw;
+        align-items: center;
+        min-height: 75vh;
+        word-wrap: break-word;
+        word-break: break-all;
+        overflow: hidden;
+    }
+
+    .mg_pg {
+        margin-left: 2vw;
+        margin-top: 1.2vh;
+    }
+
+    .search {
+        width: 10vw;
+        margin-left: 2vw;
+        height: 100%;
+    }
+
+</style>
