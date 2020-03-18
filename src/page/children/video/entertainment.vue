b3385b264279194f65a2764e92331d818a50db0d
tsontang <tsontang@jidouauto.com>
2020-03-18 14:57:52 +0800

    mh页面修改

--- src/page/children/video/entertainment.vue
+++ src/page/children/video/entertainment.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="etRoot">
         <div style="width: 8vw;">
+            <div class="et_search_rt">
+                <el-button icon="el-icon-search" class="et_search_bt" circle></el-button>
+            </div>
             <el-menu :default-active="activeIndex" mode="vertical" @select="handleSelect" background-color="#fff"
                      text-color="#000" active-text-color="#E6A23C" class="et_rt_tab">
                 <el-menu-item index="1">漫画</el-menu-item>
@@ -65,15 +68,26 @@
     .et_rt_tab {
         width: 4vw;
         height: auto;
-        margin-top: 5vh;
+        margin-top: 2vh;
         box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-        margin-left: 4.8vw;
+        margin-left: 4.7vw;
     }
 
     .et_ct_rt {
         width: 90vw;
         margin-top: 1.4vh;
         height: 86vh;
+    }
+
+    .et_search_rt {
+        width: 7.8vw;
+        display: flex;
+        flex-direction: row-reverse;
+        margin-top: 3vh;
+    }
+
+    .et_search_bt {
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
     }
 
 </style>
